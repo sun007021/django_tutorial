7c0f337398a700957e2e2d82e7b39ccf62d33b19
sun007021 <sun007021@gmail.com>
2022-06-22 18:10:56 +0900

    tutorial part2 admin 전까지 완료

--- polls/apps.py
+++ polls/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class TutorialappConfig(AppConfig):
+class PollsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'tutorialApp'
+    name = 'polls'
