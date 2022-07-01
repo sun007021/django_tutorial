24c33f68efde99e1ee4e1ebd8bcdd8e1a6561d4d
sun007021 <sun007021@gmail.com>
2022-07-01 16:42:30 +0900

    tutorial part5 polls app test 완료

--- polls/models.py
+++ polls/models.py
@@ -3,6 +3,8 @@ from django.db import models
 from django.utils import timezone
 
 # Create your models here.
+
+
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
@@ -11,7 +13,8 @@ class Question(models.Model):
         return self.question_text
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now()-datetime.timedelta(days=1)
+        now = timezone.now()
+        return now >= self.pub_date >= now-datetime.timedelta(days=1)
 
 
 class Choice(models.Model):
